22453e112cae5d9da11f6e6d06058b295ede0b9e
mdharoonhussain <haroonhussain97@gmail.com>
2024-07-16 05:38:09 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
 import './App.css'; 
 import Login from './Components/Login/Login';
-import Navbar from './Components/Nav_bar/Navbar';
+import Navbar from './Components/Navbar/Navbar';
 import { Landing_Page } from './Components/Landing_Page/Landing_Page';
 import Sign_up from './Components/Sign_up/Sign_up';
 import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-//export const API_URL = window.location.hostname === "localhost" ? "https://ritikaj-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "https://ritikaj-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/"
+//export const API_URL = window.location.hostname === "localhost" ? "https://haroonmd-5500.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "https://haroonmd-5500.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/"
 export const API_URL = window.location.origin
 console.log(
     "API_URL :",
